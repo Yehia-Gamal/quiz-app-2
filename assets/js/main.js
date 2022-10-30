a10f332670024f0a8753a7bd700eea6daac5d5bd
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-10-30 12:26:01 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+const quizData = [
+  {
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "b",
+  },
+  {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+  },
+  {
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+];
+
+let quiz = document.getElementById("quiz");
+let answeEls = document.querySelectorAll(".answer");
+let questionEl = document.getElementById("question");
+let a_text = document.getElementById("a_text");
+let b_text = document.getElementById("b_text");
+let c_text = document.getElementById("c_text");
+let d_text = document.getElementById("d_text");
+let submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  let currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerHTML = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answeEls.forEach((answeEl) => (answeEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+
+  answeEls.forEach((answeEl) => {
+    if (answeEl.checked) {
+      answer = answeEl.id;
+    }
+  });
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  let answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+        <h2 class="p-2 pt-3 pb-3 text-center">You Answered ${score}/${quizData.length} Questions Correctly</h2>
+
+        <button onclick="location.reload()" class="btn btn-primary rounded-0 w-100 p-3">Reload</button>
+      `;
+    }
+  }
+});
